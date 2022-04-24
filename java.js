a570acc77cb5f964f03773ca3c5dc15ed2821dbb
AsmaaSaadat <104289052+AsmaaSaadat@users.noreply.github.com>
2022-04-24 06:04:27 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+window.alert("Welcome to ANGLE");
+function ShopNow()
+{
+   window.alert("You can visit categories section and select your items");
+}
+function Contact()
+{
+    window.alert("You can contact us through Email");
+    
+}
+function Check()
+{
+    window.alert("Wait for your sparkle");
+    
+}
